d3d58e2d5ba579213d5744414ac6adb14ba9569c
Alessio Sardaro <sardaro.alessio@gmail.com>
2023-02-05 09:10:05 +0100

    version write automatic fro pom

--- src/sa/fx/draugths/screen/PresentationScreen.java
+++ src/sa/fx/draugths/screen/PresentationScreen.java
@@ -1,9 +1,14 @@
 package sa.fx.draugths.screen;
 
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+
 import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.text.Text;
+import sa.fx.draugths.BCDraugthsApp;
 
 public abstract class PresentationScreen extends Parent {
 
@@ -12,7 +17,18 @@ public abstract class PresentationScreen extends Parent {
 	protected Canvas c;
 	protected Text start;
 	protected int level;
-
+	protected Properties prop;
+	
+	private void loadProperties() {
+    	prop= new Properties();
+    	try {
+			prop.load(ClassLoader.getSystemClassLoader().getResourceAsStream("version.properties"));
+		} catch (IOException e) {
+			BCDraugthsApp.log.log(Level.SEVERE,"Exception:",e);
+			e.printStackTrace();
+		}
+		
+	}
 	protected PresentationScreen(String image) {
 		super();
 		c = new Canvas(800, 840);
@@ -20,6 +36,7 @@ public abstract class PresentationScreen extends Parent {
 		widthScreen = 800;
 		heightScreen = 840;
 		this.level = 0;
+		loadProperties();
 		drawScreen(image);
 
 	}
--- src/sa/fx/draugths/screen/StartScreen.java
+++ src/sa/fx/draugths/screen/StartScreen.java
@@ -12,6 +12,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+import java.util.Properties;
+import sa.fx.draugths.BCDraugthsApp;
+import java.io.IOException;
+import java.util.logging.Level;
 
 /**
  *
@@ -24,22 +28,35 @@ public class StartScreen extends PresentationScreen {
 	public StartScreen() {
 		super("startScreen2.png");
 
-	}
 
+	
+
+	}
+	
+	
 	public void drawScreen(String image) {
 		Image images = null;
 		images = new Image(image);
 		c.getGraphicsContext2D().setEffect(null);
 		c.getGraphicsContext2D().drawImage(images, 0, 0,widthScreen,heightScreen);
 
+		Font f2 = new Font(null, 10);
+		Text version = new Text("version:"+prop.getProperty("checkers.version"));
+		version.setX((widthScreen-150 ) / 2);
+		version.setY(heightScreen-50 );
+		
+		version.setFont(f2);
+		version.setFill(Color.ANTIQUEWHITE);
+		
 		Font f = new Font(null, 20);
 		start = new Text("- CLICK TO START -");
 		start.setX((widthScreen - 180) / 2);
 		start.setY(heightScreen / 2 + 50);
-
+		
 		start.setFont(f);
 		start.setFill(Color.ANTIQUEWHITE);
 		getChildren().add(start);
+				getChildren().add(version);
 		Animation t = new Transition() {
 			{
 				setCycleCount(Animation.INDEFINITE);
